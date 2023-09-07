192112e1195f46467430070c22dc4e949fcceb4c
James Slaughter <jsworldwebstudio@gmail.com>
2023-09-07 15:26:09 -0500

    Trying to correct CORS error 09/07 3

--- src/services/cart/index.js
+++ src/services/cart/index.js
@@ -21,7 +21,7 @@ export const addToCart = async (formData) => {
 
 export const getAllCartItems = async (id) => {
   try {
-    const res = await fetch(`https://eshop-full-jb27tq5s6-jsworldwebstudio-gmailcom.vercel.app/api/cart/all-cart-items?id=${id}`, {
+    const res = await fetch(`http://localhost:3000/api/cart/all-cart-items?id=${id}`, {
       method: "GET",
       headers: {
         Authorization: `Bearer ${Cookies.get("token")}`,
